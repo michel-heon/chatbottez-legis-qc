b9e1f7095477209e5c0ab126017983ff66d27974
michel-heon <heon.michel@uqam.ca>
2025-05-23 15:04:28 -0400

    Version 1.0.7 avec nouveau prompt et ajout de la commande reste

--- src/index.js
+++ src/index.js
@@ -16,27 +16,10 @@ const server = expressApp.listen(process.env.port || process.env.PORT || 3978, (
 });
 
 // Listen for incoming requests.
-//expressApp.post("/api/messages", async (req, res) => {
-//  // Route received a request to adapter for processing
-//  await adapter.process(req, res, async (context) => {
-//    // Dispatch to application for routing
-//    await app.run(context);
-//  });
-//});
 expressApp.post("/api/messages", async (req, res) => {
+  // Route received a request to adapter for processing
   await adapter.process(req, res, async (context) => {
-    const text = (context.activity.text || "").trim().toLowerCase();
-    if (text === "clear") {
-      // 1) Purge l’historique de la conversation
-      if (adapter.conversationState) {
-        await adapter.conversationState.clear(context);
-        await adapter.conversationState.saveChanges(context, true);
-      }
-      // 2) Confirmation à l’utilisateur
-      await context.sendActivity("✅ Conversation réinitialisée ! Comment puis-je vous aider maintenant ?");
-    } else {
-      // comportement normal
-      await app.run(context);
-    }
+    // Dispatch to application for routing
+    await app.run(context);
   });
 });
